2abf7efd8b80a7ad0953cf5a8b72c84b91a7b182
Cyntheea <islacynthai@yahoo.com>
2023-04-27 06:48:19 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for the program
+ *
+ * Description: Print programming is like building a multilingual puzzle.
+ *
+ * Return: Displays a message to the terminal.
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
